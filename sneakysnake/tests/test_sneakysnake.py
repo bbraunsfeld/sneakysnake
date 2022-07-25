89c24fcd5ee380497b981686dd0c8faa869f74b4
bbraunsfeld <b.braunsfeld@gmail.com>
2022-07-25 19:55:20 +0200

    fix tests

--- sneakysnake/tests/test_sneakysnake.py
+++ sneakysnake/tests/test_sneakysnake.py
@@ -7,16 +7,16 @@ import sys
 import pytest
 
 import sneakysnake
-#from sneakysnake import game_loop
+from sneakysnake.game_loop import Button
 
 
 def test_sneakysnake_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "sneakysnake" in sys.modules
 
-#def test_button_hover():
-#    """tests if class Buttom has a method hover"""
-#    assert hasattr(game_loop.Button, 'hover') == True
+def test_button_hover():
+    """tests if class Buttom has a method hover"""
+    assert hasattr(Button(), 'hover') == True
 
 #def test_button_invis():
 #    """test if class Button has method invisible"""
